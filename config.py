5e0697929fa9b737a23100d013d73cb36b63625a
amirreza272 <amirrezapiri99@gmail.com>
2026-06-29 17:40:06 +0330

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = "change-this-to-a-random-secret"
+    SECRET_KEY = "SECRET_KEY"
 
     SQLALCHEMY_DATABASE_URI = (
         "sqlite:///" + os.path.join(BASE_DIR, "database.db")
